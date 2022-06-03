e9f6d8737ced8f61800f9fec379031c9f269269b
Gustavo Flores <55601525+gflorescs@users.noreply.github.com>
2022-06-03 12:54:18 -0700

    Updates

---
+++ precache-manifest.8829fe170dfeab2450b70a401d379086.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "74c9222529ef8900b669",
+    "url": "/portfolio/static/css/main.cc6afa73.chunk.css"
+  },
+  {
+    "revision": "74c9222529ef8900b669",
+    "url": "/portfolio/static/js/main.74c92225.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/portfolio/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "e6cdbbcccb02046a28a4",
+    "url": "/portfolio/static/css/2.745d1f9d.chunk.css"
+  },
+  {
+    "revision": "e6cdbbcccb02046a28a4",
+    "url": "/portfolio/static/js/2.e6cdbbcc.chunk.js"
+  },
+  {
+    "revision": "2f3df759ab8aca3aff6c",
+    "url": "/portfolio/static/js/runtime~main.2f3df759.js"
+  },
+  {
+    "revision": "226f91dd1437c1aa5bfa5802f1b8f4f1",
+    "url": "/portfolio/static/media/Logo.226f91dd.png"
+  },
+  {
+    "revision": "6304128332371621511ea1adfdb6e6b7",
+    "url": "/portfolio/static/media/PortfolioPicture.63041283.jpg"
+  },
+  {
+    "revision": "52cb33282164ba104a3f74853d67d759",
+    "url": "/portfolio/static/media/KeyboardlessKeyboard.52cb3328.png"
+  },
+  {
+    "revision": "16e9f44cd4917a587e503b8b12d48dcb",
+    "url": "/portfolio/static/media/Multi-Tweet.16e9f44c.png"
+  },
+  {
+    "revision": "ba20563d1e7764280cc92fe6cbe652e6",
+    "url": "/portfolio/static/media/P2PChat.ba20563d.png"
+  },
+  {
+    "revision": "4432cd4f183e1fdec1171d7b1f29787f",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3342e6ecd442d2b161035095e598aafd.js"
+  "/portfolio/precache-manifest.8829fe170dfeab2450b70a401d379086.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
