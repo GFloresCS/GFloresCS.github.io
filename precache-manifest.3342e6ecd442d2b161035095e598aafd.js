98995cf8294b271e760570c29838c3c090cdc0e4
Gustavo Flores <55601525+gflorescs@users.noreply.github.com>
2022-06-03 12:40:12 -0700

    Updates

---
+++ precache-manifest.3342e6ecd442d2b161035095e598aafd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "74c9222529ef8900b669",
+    "url": "/static/css/main.cc6afa73.chunk.css"
+  },
+  {
+    "revision": "74c9222529ef8900b669",
+    "url": "/static/js/main.74c92225.chunk.js"
+  },
+  {
+    "revision": "d6ad0681366502a3b3ea",
+    "url": "/static/js/1.d6ad0681.chunk.js"
+  },
+  {
+    "revision": "e6cdbbcccb02046a28a4",
+    "url": "/static/css/2.745d1f9d.chunk.css"
+  },
+  {
+    "revision": "e6cdbbcccb02046a28a4",
+    "url": "/static/js/2.e6cdbbcc.chunk.js"
+  },
+  {
+    "revision": "1d5addb8a819dfdd6347",
+    "url": "/static/js/runtime~main.1d5addb8.js"
+  },
+  {
+    "revision": "226f91dd1437c1aa5bfa5802f1b8f4f1",
+    "url": "/static/media/Logo.226f91dd.png"
+  },
+  {
+    "revision": "6304128332371621511ea1adfdb6e6b7",
+    "url": "/static/media/PortfolioPicture.63041283.jpg"
+  },
+  {
+    "revision": "52cb33282164ba104a3f74853d67d759",
+    "url": "/static/media/KeyboardlessKeyboard.52cb3328.png"
+  },
+  {
+    "revision": "16e9f44cd4917a587e503b8b12d48dcb",
+    "url": "/static/media/Multi-Tweet.16e9f44c.png"
+  },
+  {
+    "revision": "ba20563d1e7764280cc92fe6cbe652e6",
+    "url": "/static/media/P2PChat.ba20563d.png"
+  },
+  {
+    "revision": "de4f2be991ad3e9166da856f51fba31b",
+    "url": "/index.html"
+  }
+];
